36fec9b789d16879fa40699ecc7142f63fc2d3fc
suyeong.hong <suyeong.hong@linecorp.com>
2022-12-21 19:37:42 +0900

    Set up to publish to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id("signing")
+    id("maven-publish")
 }
 
-group = "com.linecorp"
-version = "1.0-SNAPSHOT"
+group = "com.linecorp.conditional"
+version = "0.9.0"
 
 repositories {
     mavenCentral()
@@ -18,4 +20,55 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+signing {
+    sign(publishing.publications)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            repositories {
+                maven {
+                    url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    val sonatypeUsername: String? by project
+                    val sonatypePassword: String? by project
+                    credentials {
+                        username = sonatypeUsername ?: ""
+                        password = sonatypePassword ?: ""
+                    }
+                }
+            }
+            pom {
+                name.set("conditional")
+                description.set("Conditional is a super lightweight library that helps you make conditional expressions.")
+                url.set("https://github.com/line/conditional")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("icepeppermint")
+                        name.set("Suyeong Hong")
+                        email.set("suyeong.hong@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/line/conditional.git")
+                    developerConnection.set("scm:git:ssh://github.com/line/conditional.git")
+                    url.set("https://github.com/line/conditional")
+                }
+            }
+        }
+    }
 }
