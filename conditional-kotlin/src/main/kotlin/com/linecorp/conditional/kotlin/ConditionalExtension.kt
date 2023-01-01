7a5bd254e36ed8b4bd8c90847c31e96722566260
suyeong.hong <suyeong.hong@linecorp.com>
2023-01-01 18:42:39 +0900

    Change the function order of ConditionalExtension

--- conditional-kotlin/src/main/kotlin/com/linecorp/conditional/kotlin/ConditionalExtension.kt
+++ conditional-kotlin/src/main/kotlin/com/linecorp/conditional/kotlin/ConditionalExtension.kt
@@ -20,33 +20,6 @@ import com.linecorp.conditional.*
 import java.util.concurrent.TimeUnit
 
 /**
- * Returns a newly created [ComposedCondition].
- * This [ComposedCondition] is composed with the AND operator.
- *
- * @param condition the [Condition] to compose.
- *
- * @throws NullPointerException if the [condition] is null.
- */
-infix fun Condition.and(condition: Condition): ComposedCondition = this.and(condition)
-
-/**
- * Returns a newly created [ComposedCondition].
- * This [ComposedCondition] is composed with the OR operator.
- *
- * @param condition the [Condition] to compose.
- *
- * @throws NullPointerException if the [condition] is null.
- */
-infix fun Condition.or(condition: Condition): ComposedCondition = this.or(condition)
-
-/**
- * Returns a newly created negative [Condition].
- *
- * @throws NullPointerException if the [condition] is null.
- */
-operator fun Condition.not(): Condition = Condition.not(this)
-
-/**
  * Returns a newly created [Condition].
  *
  * @param function the function to match the conditional expression.
@@ -101,6 +74,33 @@ fun `true`(): Condition = Condition.trueCondition()
  * Returns a newly created [Condition] with false.
  */
 fun `false`(): Condition = Condition.falseCondition()
+
+/**
+ * Returns a newly created [ComposedCondition].
+ * This [ComposedCondition] is composed with the AND operator.
+ *
+ * @param condition the [Condition] to compose.
+ *
+ * @throws NullPointerException if the [condition] is null.
+ */
+infix fun Condition.and(condition: Condition): ComposedCondition = this.and(condition)
+
+/**
+ * Returns a newly created [ComposedCondition].
+ * This [ComposedCondition] is composed with the OR operator.
+ *
+ * @param condition the [Condition] to compose.
+ *
+ * @throws NullPointerException if the [condition] is null.
+ */
+infix fun Condition.or(condition: Condition): ComposedCondition = this.or(condition)
+
+/**
+ * Returns a newly created negative [Condition].
+ *
+ * @throws NullPointerException if the [condition] is null.
+ */
+operator fun Condition.not(): Condition = Condition.not(this)
 
 /**
  * Returns a newly created [ConditionContext].
