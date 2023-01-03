ec7c57dc78fd718b641ccb683b3f3f6d8acf1d52
suyeong.hong <suyeong.hong@linecorp.com>
2023-01-03 16:30:01 +0900

    Change http to https in License URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ subprojects {
                     licenses {
                         license {
                             name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
                         }
                     }
                     developers {
