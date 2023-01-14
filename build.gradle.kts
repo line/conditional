4347f3e90239498aa6ce181b91245fd041edd860
suyeong.hong <suyeong.hong@linecorp.com>
2023-01-14 17:38:42 +0900

    Bump junit-jupiter from 5.9.0 to 5.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ subprojects {
 
     dependencies {
         implementation("com.google.code.findbugs:jsr305:3.0.2")
-        testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
         testImplementation("org.assertj:assertj-core:3.23.1")
         testImplementation("org.awaitility:awaitility:4.2.0")
     }
